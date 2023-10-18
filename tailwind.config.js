4402dc60f963da66e3d33a45df5d8578fc3af55f
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2023-10-18 22:41:57 +0300

    Added variable colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,9 +34,56 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('./images/hero-1280@2x.jpg')",
+        'hero-pattern': "url('./images/hero-01-1280@2x.jpg')",
         // 'footer-texture': "url('/img/footer-texture.png')",
-      }
+      },
+
+      colors: {
+        
+
+        
+        // main: "#9D7558",
+       
+
+        accent1: "#9D7558",
+        // accent3: "#9E71AB",
+        
+        // disabled: "#BC9DC5",
+        
+        // red: "#E7414B",
+        // lightGrey: "#6b7280",
+        lightGrey: "#6b7280",
+        
+       
+        // bg: "#272727",
+        bgDark: "#272727",
+        // shadow: "#AB80B8",
+
+        // // uwp
+        // // main: "#FF304D",
+        // light: "#F8F8F8",
+        // dark: "#171717",
+        // grey: "#323232",
+        // // lightGrey: "#E0E0E0",
+        // lightDarkGrey: "#909090",
+        // // middleGrey: "#919191",
+        // lightRed: "#FF445E",
+        // darkGrey: "#474747",
+        // activeRed: "#DC2D45",
+
+        // link: "#4B73FF",
+        // linkActive: "#86A1FF",
+        // formBg: "#EEEEEE",
+        // formBgDark: "#202020",
+        // dynamicBg: "#FF304DB2",
+        // langBg: "#262626",
+        // scrollBar: "#F9D4D9",
+        // bgColorBtnForm: "#535353",
+        // mainTextBlue: "#6355ff",
+
+
+      },
+
     },
     
   },
